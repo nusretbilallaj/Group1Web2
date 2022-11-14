1b16254b676129a9942fd782351bdaef95b81c49
Nusret Bilallaj <netktit@gmail.com>
2022-11-14 18:55:17 +0100

    Add project files.

---
+++ Group1Web2/Controllers/ProfesorController.cs
@@ -0,0 +1,24 @@
+﻿using Group1Web2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group1Web2.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        public IActionResult Index()
+        {
+            Group1Web2.Models.Profesori prof1 = new Profesori("Leron", "Berisha");
+            Profesori prof2 = new Profesori("Shkelqim", "Miftari");
+            List<Profesori> profesoret = new List<Profesori>();
+            profesoret.Add(prof1);
+            profesoret.Add(prof2);
+            return View(profesoret);
+        }
+
+        public IActionResult MesoHtml()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Group1Web2/Models/Profesori.cs
@@ -0,0 +1,14 @@
+﻿namespace Group1Web2.Models
+{
+    public class Profesori
+    {
+        public Profesori(string emri,string mbiemri)
+        {
+            Emri=emri;
+            Mbiemri=mbiemri;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+    }
+}
